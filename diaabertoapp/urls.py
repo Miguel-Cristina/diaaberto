94fbecc44da871ecc0baea184075ac99b6bcbc99
Miguel Cristina <a54375@ualg.pt>
2020-03-08 02:05:47 +0000

    add atividades.html, update views.py, urls.py add admin/

--- diaabertoapp/urls.py
+++ diaabertoapp/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
+from diaabertoapp.views import index, atividades
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('index/', views.index, name='index'),
+    path('atividades/', views.atividades, name='atividades'),
 ]
--- diaabertoapp/views.py
+++ diaabertoapp/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'diaabertoapp/index.html', {})
+
+def atividades(request):
+    return render(request, 'diaabertoapp/atividades.html', {})
