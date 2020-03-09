8fd25e8f53154081fab2fdd6b2a299180fb1900c
Miguel Cristina <a54375@ualg.pt>
2020-03-09 21:50:14 +0000

    Add few changes do admin.py

--- diaabertoapp/admin.py
+++ diaabertoapp/admin.py
@@ -1,8 +1,49 @@
 from django.contrib import admin
-from diaabertoapp.models import Edificio, Campus, LocalAtividade, Atividade
+from diaabertoapp.models import Edificio, Campus, LocalAtividade, Tematica, Atividade
+from django.utils.safestring import mark_safe
 
 # Register your models here.
-admin.site.register(Edificio)
-admin.site.register(Campus)
-admin.site.register(LocalAtividade)
-admin.site.register(Atividade)
+
+class CampusAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'morada', 'contacto')
+
+admin.site.register(Campus, CampusAdmin)
+
+class LocalAtividadeAdmin(admin.ModelAdmin):
+    list_display = ('sala','andar', 'get_edificio', 'descricao', 'get_campus')
+
+    def get_campus(self, obj):
+        return obj.edificio.campus.nome
+    get_campus.short_description = "Campus"
+    def get_edificio(self, obj):
+        return obj.edificio.nome
+    get_edificio.short_description = "Edificio"
+
+admin.site.register(LocalAtividade, LocalAtividadeAdmin)
+
+admin.site.register(Tematica)
+
+
+class EdificioAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'campus')
+
+admin.site.register(Edificio, EdificioAdmin)
+
+class AtividadeAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'descricao', 'local', 'duracao', 'limite_participantes', 'tipo_atividade', 'publico_alvo', 'colored_name')
+    def colored_name(self,obj):
+        if obj.validada == 'Validada':
+            estado = 'Validada'
+            cor = 'rgb(179, 255, 153)'
+        elif obj.validada == 'Rejeitada':
+            estado = 'Rejeitada'
+            cor = 'rgb(255, 153, 153)'
+        else:
+            estado = 'Pendente'
+            cor = '	rgb(255, 255, 153)'
+        return mark_safe('<div class="container" style="border-radius:5px;color:black; background:{};">{}</div>'.format(cor,estado))
+    colored_name.allow_tags = True
+    colored_name.short_description = 'Estado validação'
+    colored_name.admin_order_field = 'validada'
+
+admin.site.register(Atividade, AtividadeAdmin)
--- diaabertoapp/models.py
+++ diaabertoapp/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+import datetime
 # Create your models here.
 
 
@@ -28,7 +28,7 @@ class Edificio(models.Model):
         return reverse('edificio-detail-view',args=[str(self.id)])
 
     def __str__(self):
-        return '(' + self.campus.nome + ') '+  self.nome
+        return self.nome + ' ('+ self.campus.nome + ')'
 
 class LocalAtividade(models.Model):
     id = models.AutoField(primary_key=True)
@@ -43,16 +43,81 @@ class LocalAtividade(models.Model):
     def __str__(self):
         return self.edificio.campus.nome + ' Edificio ' + self.edificio.nome + ' Piso ' +self.andar + ' Sala ' + self.sala
 
+class Tematica(models.Model):
+    # Campos
+    id = models.AutoField(primary_key=True)
+    tema = models.CharField(max_length=255, unique=True)
+    
+    #Métodos
+    def get_absolute_url(self):
+        return reverse('tematica-detail-view',args=[str(self.id)])
+
+    def __str__(self):
+        return self.tema
+
 class Atividade(models.Model):
+ 
     id = models.AutoField(primary_key=True)
     local = models.ForeignKey(LocalAtividade, on_delete=models.CASCADE,null=True) 
     nome = models.CharField(max_length=255)
     descricao = models.TextField(null=True)
     duracao = models.IntegerField()
     limite_participantes = models.IntegerField()
-    validada = models.IntegerField()
-    tipo_atividade = models.CharField(max_length=255)
-    public_alvo = models.CharField(max_length=45)
+
+    VISITASINSTALACAO = 'VI' 
+    VISITASLABORATORIO = 'VL'   
+    ATIVIDADESEXPERIMENTAIS = 'AE'    
+    ATIVIDADESTECNOLOGICAS = 'AT'
+    FEIRACIENCIAS = 'FC'
+    PALESTRAS = 'PL'
+    CONFERENCIAS = 'CF'
+    SEMINARIOS = 'SM'
+    TERTULIAS = 'TT'
+    OUTRASENSINO = 'OE'
+    OUTRASCULTURAIS = 'OC'
+    OUTRASDESPORTIVAS = 'OD'
+    EXPOSICOES = 'EX'
+    PASSEIOS = 'PS'
+    OUTRAS = 'OU'
+    TIPO_ATIVIDADE_CHOICES = [
+        (VISITASINSTALACAO, 'Visitas Instalações'),
+        (VISITASLABORATORIO , 'Visitas Laboratórios'),
+        (ATIVIDADESEXPERIMENTAIS , 'Atividades Experimentais'),
+	    (ATIVIDADESTECNOLOGICAS ,'Atividades Tencnológicas'),
+	    (FEIRACIENCIAS ,'Feira das Ciências'),
+	    (PALESTRAS ,'Palestras'),
+	    (CONFERENCIAS ,'Conferências'),
+	    (SEMINARIOS ,'Seminários'),
+	    (TERTULIAS ,'Tertúlias'),
+	    (OUTRASENSINO ,'Outras Atividades (Ensino)'),
+	    (OUTRASCULTURAIS ,'Outras Atividades (Culturais)'),
+	    (OUTRASDESPORTIVAS ,'Outras Atividades (Desportivas)'),
+	    (EXPOSICOES ,'Exposições'),
+	    (PASSEIOS ,'Passeios'),
+	    (OUTRAS ,'Outras Atividades'),
+    ]
+    tipo_atividade = models.CharField(
+        max_length=2,
+        choices=TIPO_ATIVIDADE_CHOICES,
+    )
+    publico_alvo = models.CharField(max_length=45)
+    data = models.DateField(default=datetime.date.today)
+    
+    REJEITADA = 'Rejeitada'    #invalidada
+    PENDENTE = 'Pendente'     #por validar
+    VALIDADA = 'Validada'     #validada
+    VALIDACAO_CHOICES = [
+        (REJEITADA, 'Rejeitada'),
+        (PENDENTE, 'Pendente'),
+        (VALIDADA, 'Validada'),
+    ]
+    validada = models.CharField(
+        max_length=32,
+        choices=VALIDACAO_CHOICES,
+        default=PENDENTE,
+    )
+    
+    #validada = models.BooleanField()
 
     #Métodos
     def get_absolute_url(self):
