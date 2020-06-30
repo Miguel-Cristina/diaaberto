21536363c7e247c9d130392c400977c5fa4406ef
rui35 <rui35@LAPTOP-56NM0C81>
2020-06-30 16:42:43 +0100

    rui

--- diaaberto/settings.py
+++ diaaberto/settings.py
@@ -98,7 +98,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'test5',
         'USER': 'root',
-        'PASSWORD': 'administrator',
+        'PASSWORD': 'root',
         'HOST': '127.0.0.1',
         'PORT': '3306'
     }
